cb4f4e02bbbc5bd4a9720fb788938064f62e289a
PedroCarlucci <62573594+PedroCarlucci@users.noreply.github.com>
2026-03-20 12:38:43 -0300

    Config setup

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // │  Preencha com seus dados do projeto                     │
 // └─────────────────────────────────────────────────────────┘
 
-const SUPABASE_URL      = 'COLE_SUA_URL_AQUI';
-const SUPABASE_ANON_KEY = 'COLE_SUA_CHAVE_AQUI';
+const SUPABASE_URL = 'https://fsgdsrdgocaokmuifzpm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzZ2RzcmRnb2Nhb2ttdWlmenBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MjI4NTEsImV4cCI6MjA4OTQ5ODg1MX0.vlx8wF4bs2WR8kOMhS3qtqMw-dDY-yK_xR8oRmVcCN4';
